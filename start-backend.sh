7f0fbc5884518ad20b487499ce5def51ba237097
daum88 <68275432+daum88@users.noreply.github.com>
2026-03-27 12:52:08 +0200

    Remove remaining markdown and shell script files

--- start-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Käivitan backendit..."
-echo "Backend URL: http://localhost:5000"
-echo "Swagger UI: http://localhost:5000/swagger"
-echo ""
-
-cd backend
-dotnet run --launch-profile http
